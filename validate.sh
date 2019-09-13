57a9b7961daa03cd249a14bf1cfe358d13a2a82f
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2019-09-13 16:03:36 -0400

    Update

--- validate.sh
+++ validate.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-java -jar ~/Applications/womtool-40.jar \
+java -jar ~/Applications/womtool-45.1.jar \
     validate \
     sharp.wdl \
     --inputs sharp.inputs.json
+
+java -jar ~/Applications/womtool-45.1.jar \
+    validate \
+    fastqc.wdl \
+    --inputs fastqc.inputs.json
