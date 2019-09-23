bd998404269a02860ffd2e6e143c57903744a392
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2019-09-23 13:38:34 -0400

    Use version-agnostic jar file

--- validate.sh
+++ validate.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
-java -jar ~/Applications/womtool-45.1.jar \
+java -jar ~/Applications/womtool.jar \
     validate \
     sharp.wdl \
     --inputs sharp.inputs.json
 
-java -jar ~/Applications/womtool-45.1.jar \
+java -jar ~/Applications/womtool.jar \
     validate \
     fastqc.wdl \
     --inputs fastqc.inputs.json
 
-java -jar ~/Applications/womtool-45.1.jar \
+java -jar ~/Applications/womtool.jar \
     validate \
     cutadapt.wdl \
     --inputs cutadapt.inputs.json
 
-java -jar ~/Applications/womtool-45.1.jar \
+java -jar ~/Applications/womtool.jar \
     validate \
     prep-cb-whitelist.wdl \
     --inputs prep-cb-whitelist.inputs.json
 
-java -jar ~/Applications/womtool-45.1.jar \
+java -jar ~/Applications/womtool.jar \
     validate \
     count.wdl \
     --inputs count.inputs.json
 
-java -jar ~/Applications/womtool-45.1.jar \
+java -jar ~/Applications/womtool.jar \
     validate \
     hto-demux.wdl \
     --inputs hto-demux.inputs.json
