c9914849472299e48692013d0a0c8a2df8b206e5
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2019-10-24 10:40:20 -0400

    Support labels and options

--- submit.sh
+++ submit.sh
@@ -6,20 +6,24 @@ cat << EOF
 USAGE: `basename $0` [options]
     -k  service account key (e.g. secrets.json)
     -i  inputs file (e.g. sample.inputs.json)
+    -l  labels file (e.g. labels.json)
+    -o  options file (e.g. options.json)
 EOF
 }
 
-while getopts "k:i:h" OPTION
+while getopts "k:i:l:o:h" OPTION
 do
     case $OPTION in
         k) service_account_key=$OPTARG ;;
         i) inputs_file=$OPTARG ;;
+        l) labels_file=$OPTARG ;;
+        o) options_file=$OPTARG ;;
         h) usage; exit 1 ;;
         *) usage; exit 1 ;;
     esac
 done
 
-if [ -z "$service_account_key" ] || [ -z "$inputs_file" ]
+if [ -z "$service_account_key" ] || [ -z "$inputs_file" ] || [ -z "$labels_file" ] || [ -z "$options_file" ]
 then
     usage
     exit 1
@@ -46,6 +50,7 @@ zip Sharp.deps.zip modules modules/*
 cromwell-tools submit \
     --secrets-file ${service_account_key} \
     --wdl Sharp.wdl \
-    --inputs-files ${inputs_file} \
     --deps-file Sharp.deps.zip \
-    --label-file Sharp.labels.json
+    --inputs-files ${inputs_file} \
+    --label-file ${labels_file} \
+    --options-file ${options_file}
