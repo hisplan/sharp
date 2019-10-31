7a16bcb0f009b1425de63cfff3bfd38867087cb4
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2019-10-31 13:53:49 -0400

    Add installation script

--- package.sh
+++ package.sh
@@ -10,9 +10,21 @@ s3_dest="s3://dp-lab-home/software"
 
 path_workdir=`mktemp -d`
 
+# create installation script
+cat <<EOF > ${path_workdir}/install.sh
+#!/bin/bash
+
+aws s3 cp --quiet s3://dp-lab-home/software/sharp-${version}.tar.gz .
+mkdir -p sharp-${version}
+tar xzf sharp-${version}.tar.gz -C sharp-${version}
+
+echo "DONE."
+EOF
+
 tar cvzf ${path_workdir}/sharp-${version}.tar.gz \
-    submit.sh Sharp.wdl Sharp.deps.zip Sharp.options.*.json
+    submit.sh Sharp.wdl Sharp.deps.zip Sharp.options.aws.json
 
 aws s3 cp ${path_workdir}/sharp-${version}.tar.gz ${s3_dest}/
+aws s3 cp ${path_workdir}/install.sh ${s3_dest}/install-sharp-${version}.sh
 
 rm -rf ${path_workdir}
