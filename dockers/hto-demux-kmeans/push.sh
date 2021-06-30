283f9f9d35dc9131c286a0cf82ada4a4bf73d092
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2021-06-30 08:06:53 -0400

    Set version to 0.5.0

--- dockers/hto-demux-kmeans/push.sh
+++ dockers/hto-demux-kmeans/push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker login
-docker tag cromwell-hto-demux-kmeans:0.4.0 hisplan/cromwell-hto-demux-kmeans:0.4.0
-docker push hisplan/cromwell-hto-demux-kmeans:0.4.0
+docker tag cromwell-hto-demux-kmeans:0.5.0 hisplan/cromwell-hto-demux-kmeans:0.5.0
+docker push hisplan/cromwell-hto-demux-kmeans:0.5.0
