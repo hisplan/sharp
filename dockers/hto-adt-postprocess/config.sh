ca2ba83a51c7d68c6c817626310527eba06646b4
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2021-07-26 08:33:32 -0400

    Set version to 0.3.3

--- dockers/hto-adt-postprocess/config.sh
+++ dockers/hto-adt-postprocess/config.sh
@@ -1,7 +1,8 @@
-version="0.3.2"
+version="0.3.3"
 
 # docker related
 registry="583643567512.dkr.ecr.us-east-1.amazonaws.com"
+registry="hisplan"
 image_name="cromwell-hto-adt-postprocess"
 
 # set this to 1 if you want to create a repository in AWS ECR
