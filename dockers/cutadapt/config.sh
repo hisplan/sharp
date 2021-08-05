01455a288853a76d6ae06c1c33b26a6588b3a76c
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2021-08-05 07:36:01 -0400

    Remove and use https://github.com/hisplan/docker-cutadapt

--- dockers/cutadapt/config.sh
+++
@@ -1,5 +0,0 @@
-version="2.5"
-
-# docker related
-registry="quay.io/hisplan"
-image_name="cutadapt"
--- dockers/cutadapt/push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-source config.sh
-
-echo "${registry}/${image_name}:${version}"
-
-scing push --image=${registry}/${image_name}:${version}
-scing push --image=${registry}/cromwell-${image_name}:${version}
