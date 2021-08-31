9059386bc4c2b654615865e39c3239149d7e2ec0
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2021-08-31 12:35:13 -0400

    Minor update

--- run-tests.sh
+++ run-tests.sh
@@ -3,34 +3,34 @@
 echo ">> 10x v3 / TotalSeq-A"
 ./submit-hashtag.sh \
     -k ~/keys/secrets-aws.json \
-    -i configs/hashtag-10x-v3-tsa.inputs.json \
-    -l configs/hashtag-10x-v3-tsa.labels.json \
+    -i configs/devtest/hashtag-10x-v3-tsa.inputs.json \
+    -l configs/devtest/hashtag-10x-v3-tsa.labels.json \
     -o Sharp.options.aws.json
 
 echo ">> 10x v3 / TotalSeq-B barcode translation"
 ./submit-hashtag.sh \
     -k ~/keys/cromwell-secrets-aws-nvirginia.json \
-    -i configs/hashtag-10x-v3-tsb.inputs.json \
-    -l configs/hashtag-10x-v3-tsb.labels.json \
+    -i configs/devtest/hashtag-10x-v3-tsb.inputs.json \
+    -l configs/devtest/hashtag-10x-v3-tsb.labels.json \
     -o Sharp.options.aws.json
 
 echo ">> CITE-seq"
 ./submit-citeseq.sh \
     -k ~/keys/cromwell-secrets-aws-nvirginia.json \
-    -i configs/citeseq.inputs.json \
-    -l configs/citeseq.labels.json \
+    -i configs/devtest/citeseq.inputs.json \
+    -l configs/devtest/citeseq.labels.json \
     -o Sharp.options.aws.json
 
 echo ">> ASAP-seq"
 ./submit-asapseq.sh \
     -k ~/keys/cromwell-secrets-aws-nvirginia.json \
-    -i configs/asapseq-tsa.inputs.json \
-    -l configs/asapseq-tsa.labels.json \
+    -i configs/devtest/asapseq-tsa.inputs.json \
+    -l configs/devtest/asapseq-tsa.labels.json \
     -o Sharp.options.aws.json
 
-echo ">> Cell Plex"
+echo ">> CellPlex"
 ./submit-cellplex.sh \
     -k ~/keys/cromwell-secrets-aws-nvirginia.json \
-    -i configs/cellplex.inputs.json \
-    -l configs/cellplex.labels.json \
+    -i configs/devtest/cellplex.inputs.json \
+    -l configs/devtest/cellplex.labels.json \
     -o Sharp.options.aws.json
